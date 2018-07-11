fde7c68d95d5d669ce6657144de37cc581c902d5
Corey Lutrick <coreylutrick@gmail.com>
2018-07-10 20:29:21 -0700

    can delete order from firebase now

--- src/components/SingleOrder/SingleOrder.js
+++ src/components/SingleOrder/SingleOrder.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import orderRequest from '../../firebaseRequests/orders';
 
 import './SingleOrder.css';
 
 class SingleOrder extends React.Component {
+  deleteOrderClick = () =>
+  {
+    const firebaseId = this.props.match.params.id;
+    orderRequest
+      .deleteRequest(firebaseId)
+      .then(() =>
+      {
+        this.props.history.push('/orders');
+      })
+      .catch((err) =>
+      {
+        console.error(err);
+      });
+  };
   render () {
     return (
       <div className="SingleOrder">
         <h1>SingleOrder</h1>
+        <button className="btn btn-danger" onClick={this.deleteOrderClick}>
+          Delete Order
+        </button>
       </div>
     );
   }
--- src/firebaseRequests/orders.js
+++ src/firebaseRequests/orders.js
@@ -39,4 +39,21 @@ const postRequest = (order) =>
   });
 };
 
-export default {getRequest, postRequest};
+const deleteRequest = (orderId) =>
+{
+  return new Promise((resolve, reject) =>
+  {
+    axios
+      .delete(`${constants.firebaseConfig.databaseURL}/orders/${orderId}.json`)
+      .then((res) =>
+      {
+        resolve(res);
+      })
+      .catch((err) =>
+      {
+        reject(err);
+      });
+  });
+};
+
+export default {getRequest, postRequest, deleteRequest};
