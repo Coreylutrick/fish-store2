d420e285d2656ec42a1d6edd198e7dd6afc28e51
Corey Lutrick <coreylutrick@gmail.com>
2018-07-10 21:15:45 -0700

    can save the modified order to firebase now

--- src/components/SingleOrder/SingleOrder.js
+++ src/components/SingleOrder/SingleOrder.js
@@ -42,6 +42,21 @@ class SingleOrder extends React.Component {
       }));
   }
 
+  updateOrderClick = () =>
+  {
+    const firebaseId = this.props.match.params.id;
+    orderRequests
+      .putRequest(firebaseId, this.state.order)
+      .then(() =>
+      {
+        this.props.history.push('/orders');
+      })
+      .catch((err) =>
+      {
+        console.error(err);
+      });
+  };
+
   deleteOrderClick = () => {
     const firebaseId = this.props.match.params.id;
     orderRequests
@@ -111,7 +126,7 @@ class SingleOrder extends React.Component {
         </div>
         <div>
           <div className="col-xs-6">
-            <button className="col-xs-12 btn btn-default">
+            <button className="col-xs-12 btn btn-default" onClick={this.updateOrderClick}>
               Update Order
             </button>
           </div>
